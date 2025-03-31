d16e6b33be8f7a52635e0ca2d8fb3c397846c20e
mikle1978-mo <azimuth-m@yandex.ru>
2025-03-31 12:38:20 +0300

    250331

--- components/UI/Modal.jsx
+++ components/UI/Modal.jsx
@@ -1,4 +1,4 @@
-import styles from "./modal.module.css";
+import styles from "./Modal.module.css";
 
 export default function MyModal({ isOpen, toggleOpen, children }) {
     const handleModalClick = (e) => {
